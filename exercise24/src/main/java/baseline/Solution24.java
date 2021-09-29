31dd2a9d19797aff8abf1ca0e702ee14897a2875
Miguel <miguelangel53013@gmail.com>
2021-09-29 01:03:15 -0400

    committing psuedocode skeleton

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Miguel Rosario
+ */
+package baseline;
+
+public class Solution24 {
+    public static void main (String[] args){
+        /*
+        Declare 2 Strings word1 and word2
+        Ask the user to enter the string for word1
+            word1 = getUserWord("Enter the first string:")
+        Ask the user to enter the string for word2
+            word2 = getUserWord("Enter the second string:")
+        Compare the two words to see if they are anagrams
+            result = isAnagram()
+        Print out the result
+        */
+    }
+
+    public String getUserWord(String prompt){
+        // print input prompt
+        // read in String data
+        // return String
+        return NULL;
+    }
+
+    public void printResult(boolean result){
+        // Checks result
+        // if result is false, print that the words are not anagrams
+        // otherwise print that the words are anagrams
+    }
+
+    public boolean isAnagram(String word1, String word2){
+        /*  take length of word1
+            take length of word2
+            if the length of both do not equal, return false
+            otherwise loop to the length of the words
+                compare the char at word1[i] and word2[length-1-i]
+                if they are ever different, return false
+            return true
+         */
+        return true;
+    }
+}
