5c9bf6c226be4f67f724f59a75f59df20832840e
Miguel <miguelangel53013@gmail.com>
2021-09-29 01:59:29 -0400

    committing complete solution

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -4,51 +4,70 @@
  */
 package baseline;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Solution24 {
+    private static final Scanner input = new Scanner(System.in);
     public static void main (String[] args){
-        /*
-        Declare 2 Strings word1 and word2
-        Ask the user to enter the string for word1
-            word1 = getUserWord("Enter the first string:")
-        Ask the user to enter the string for word2
-            word2 = getUserWord("Enter the second string:")
-        Compare the two words to see if they are anagrams
-            result = isAnagram()
-        Print out the result
-        */
+        Solution24 app = new Solution24();
+        // Declare 2 Strings word1 and word2
+        // Ask the user to enter the strings for word1 and word2
+        String word1 = app.getUserWord("Enter the first string: ");
+        String word2 = app.getUserWord("Enter the second string: ");
+        // Compare the two words to see if they are anagrams
+        boolean result = app.isAnagram(word1, word2);
+        // Print out the result
+        app.printResult(result, word1, word2);
+
     }
 
     public String getUserWord(String prompt){
         // print input prompt
+        System.out.print(prompt);
         // read in String data
         // return String
-        return NULL;
+        return input.nextLine();
     }
 
-    public void printResult(boolean result){
+    public void printResult(boolean result, String word1, String word2){
+        // Create initial string
+        String output = "\"" + word1 + "\" and \"" + word2 + "\" are";
         // Checks result
-        // if result is false, print that the words are not anagrams
-        // otherwise print that the words are anagrams
+        // If result is false, add to the string that the words are not anagrams
+        // Otherwise add that the words are anagrams
+        if(!result){
+            output += " not anagrams.";
+        } else {
+            output += " anagrams.";
+        }
+        // Print out the result
+        System.out.println(output);
     }
 
     public boolean isAnagram(String word1, String word2){
-        /*  take length of word1
-            take length of word2
-            if the length of both do not equal, return false
-            Use alphabetSortedString to sort both strings alphabetically
-                tempW1 = alphabetSortedString(word1)
-                tempW2 = alphabetSortedString(word2)
-            compare the two strings
-            return true if the strings are equal, otherwise return false.
-         */
-        return true;
+        // take length of word1 and word2
+        int word1Len = word1.length();
+        int word2Len = word2.length();
+        // if the length of both do not equal, return false
+        if (word1Len != word2Len){
+            return false;
+        } else {
+            // Use alphabetSortedString to sort both strings alphabetically
+            String sortedWord1 = alphabetSortedString(word1);
+            String sortedWord2 = alphabetSortedString(word2);
+            // return true if the strings are equal, otherwise return false.
+            return sortedWord1.equalsIgnoreCase(sortedWord2);
+        }
     }
 
     public String alphabetSortedString(String word){
         // convert string to char array
+        char[] wordArray = word.toCharArray();
         // sort char array alphabetically
+        Arrays.sort(wordArray);
         // reconvert back into a string
         // return the string
-        return NULL;
+        return new String(wordArray);
     }
 }
