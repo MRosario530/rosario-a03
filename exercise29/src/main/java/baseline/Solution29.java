39007389c84e381c09fe315032485fc0fe34c9b1
Miguel <miguelangel53013@gmail.com>
2021-10-01 03:00:28 -0400

    committing initial files and pseudocode skeleton

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Miguel Rosario
+ */
+package baseline;
+
+public class Solution29 {
+
+    public static void main(String[] args) {
+        // Create a double rateOfReturn and call getUserDouble()
+        // call calculateDoubleInvestment(rateOfReturn) and print the result
+    }
+
+    public double getUserDouble(){
+        // Create a variable responsible for maintaining the loop
+        // Prompt the user to enter a number
+        // Attempt to read in an int using a try/catch and a loop
+        // Have a print statement within the catch for not a valid input
+        // Have an extra statement to not allow 0 as an input
+        // Loop until the user enters a valid double
+        // Return the user double
+    }
+
+    public double calculateDoubleInvestment(double rateOfReturn){
+        // Return 72.0/rateOfReturn
+    }
+}
