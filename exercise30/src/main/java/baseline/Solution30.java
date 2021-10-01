bbf3100e8e93c0f2845344bc87a17292f417691a
Miguel <miguelangel53013@gmail.com>
2021-10-01 17:25:51 -0400

    committing completed code and test cases

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -1,24 +1,49 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Miguel Rosario
+ */
 package baseline;
+
+import java.util.Formatter;
 
 public class Solution30 {
     public static void main(String[] args) {
+        Solution30 app = new Solution30();
         // Create a 2d integer array called multTable and call calculateMultiplicationTable()
-        // Create a string and call formatMultiplicationTable(multTable)
-        // Print the string
-
+        int[][] multTable = app.calculateMultiplicationTable();
+        // Call formatMultiplicationTable(multTable) in a print statement
+        System.out.print(app.formatMultiplicationTable(multTable));
     }
 
     public int[][] calculateMultiplicationTable(){
         // Create a 2d array of integers of length and width 12
+        int[][] multTable = new int[12][12];
         // Create a double for loop, multiplying each value from 1-12 with each other
-        // Place the value in the correct location of the array
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++){
+                // Place the value in the correct location of the array
+                multTable[i][j] = (i+1) * (j+1);
+            }
+        }
         // Return the array
+        return multTable;
     }
 
-    public String formatMultiplicationTable(int[][] multiplicationTable){
+    public StringBuilder formatMultiplicationTable(int[][] multiplicationTable){
         // Create a formatter object and have the output be right shifted
-        // Create a for loop, formatting every value and adding it to the string
+        StringBuilder multTableText = new StringBuilder();
+        Formatter formatter = new Formatter(multTableText);
+        // Create a double for loop, formatting every value and adding it to the string
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++){
+                formatter.format("%4d",multiplicationTable[i][j]);
+            }
             // Ensure there is a new line character after every 12 characters.
+            multTableText.append("\n");
+        }
+        // Close the formatter
+        formatter.close();
         // Return the string
+        return multTableText;
     }
 }
