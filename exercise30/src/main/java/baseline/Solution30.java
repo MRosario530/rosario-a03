84e5f84b62e51e0e46b7794de2d599d60aae6fd6
Miguel <miguelangel53013@gmail.com>
2021-10-01 19:58:13 -0400

    committing updates to code and test cases

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -9,13 +9,31 @@ import java.util.Formatter;
 public class Solution30 {
     public static void main(String[] args) {
         Solution30 app = new Solution30();
-        // Create a 2d integer array called multTable and call calculateMultiplicationTable()
-        int[][] multTable = app.calculateMultiplicationTable();
-        // Call printMultiplicationTable(multTable) to print the table
-        app.printMultiplicationTable(multTable);
+        // Call createMultiplicationTable(multTable) to print the table
+        System.out.print(app.createMultiplicationTable());
     }
 
-    public int[][] calculateMultiplicationTable(){
+    public StringBuilder createMultiplicationTable(){
+        // Create a formatter object and have the output be right shifted
+        StringBuilder multTableText = new StringBuilder();
+        Formatter formatter = new Formatter(multTableText);
+        // Create and store the multiplication table in a 2d int array
+        int[][] multiplicationTable = calculateMultiplicationTable();
+        // Create a double for loop, formatting every value and adding it to the string
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++){
+                formatter.format("%5d",multiplicationTable[i][j]);
+            }
+            // Ensure there is a new line character after every 12 characters.
+            multTableText.append("\n");
+        }
+        // Close the formatter
+        formatter.close();
+        // Return the string
+        return multTableText;
+    }
+
+    private int[][] calculateMultiplicationTable(){
         // Create a 2d array of integers of length and width 12
         int[][] multTable = new int[12][12];
         // Create a double for loop, multiplying each value from 1-12 with each other
@@ -27,23 +45,5 @@ public class Solution30 {
         }
         // Return the array
         return multTable;
-    }
-
-    public void printMultiplicationTable(int[][] multiplicationTable){
-        // Create a formatter object and have the output be right shifted
-        StringBuilder multTableText = new StringBuilder();
-        Formatter formatter = new Formatter(multTableText);
-        // Create a double for loop, formatting every value and adding it to the string
-        for (int i = 0; i < 12; i++) {
-            for (int j = 0; j < 12; j++){
-                formatter.format("%4d",multiplicationTable[i][j]);
-            }
-            // Ensure there is a new line character after every 12 characters.
-            multTableText.append("\n");
-        }
-        // Close the formatter
-        formatter.close();
-        // Return the string
-        System.out.print(multTableText);
     }
 }
