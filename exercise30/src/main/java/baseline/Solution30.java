f3b1f56cb89302ce882c94ed0ed5039f6c9d9745
Miguel <miguelangel53013@gmail.com>
2021-10-01 16:18:54 -0400

    committing initial files and pseudocode skeleton

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,24 @@
+package baseline;
+
+public class Solution30 {
+    public static void main(String[] args) {
+        // Create a 2d integer array called multTable and call calculateMultiplicationTable()
+        // Create a string and call formatMultiplicationTable(multTable)
+        // Print the string
+
+    }
+
+    public int[][] calculateMultiplicationTable(){
+        // Create a 2d array of integers of length and width 12
+        // Create a double for loop, multiplying each value from 1-12 with each other
+        // Place the value in the correct location of the array
+        // Return the array
+    }
+
+    public String formatMultiplicationTable(int[][] multiplicationTable){
+        // Create a formatter object and have the output be right shifted
+        // Create a for loop, formatting every value and adding it to the string
+            // Ensure there is a new line character after every 12 characters.
+        // Return the string
+    }
+}
