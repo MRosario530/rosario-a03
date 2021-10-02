0c53f5a538b9dab6909914dd4aa82dbfe3355d1e
Miguel <miguelangel53013@gmail.com>
2021-10-02 00:07:49 -0400

    committing initial files and pseudocode skeleton

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Miguel Rosario
+ */
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution34 {
+
+    public static void main(String[] args){
+        // Create an arraylist of 5 employees
+        // Call the method formatEmployeeList(employeeList) to display how many employees and the names of them all
+        // Prompt the user to enter an employee name to remove
+        // Call the method removeEmployee(employeeList, employeeName) and return the possibly altered arraylist
+        // Call the method formatEmployeeList(employeeList) to display the remaining employees
+    }
+
+    public String formatEmployeeList(ArrayList<String> employeeList){
+        // Create a String called employeeDisplay
+        // Add to the string "There are employeeList.size() employees"
+        // Create a for loop from 0 to employeeList.size()
+            // Add each name to the string followed by a \n
+        // Return the string employeeDisplay
+    }
+
+    public ArrayList<String> removeEmployee(String employeeList, String employeeName){
+        // Index through the arraylist to see if employeeName is present within the list
+        // If so delete the employee name
+        // Return the list
+    }
+}
