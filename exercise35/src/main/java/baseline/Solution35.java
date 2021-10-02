cae9b0fc97513b2cac82f1a66da4a60f6ac35289
Miguel <miguelangel53013@gmail.com>
2021-10-02 12:56:25 -0400

    committing initial files and pseudocode skeleton

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Miguel Rosario
+ */
+package baseline;
+
+import java.util.*;
+
+public class Solution35 {
+    public static void main (String[] args){
+        // Create an arraylist and call getContestants()
+        // Call findWinner(contestantList) and output the returned string
+    }
+
+    public ArrayList<String> getContestants(){
+        // Create a temp variable to keep the loop going
+        // Create a do while loop
+        // Prompt the user for input
+        // Take the user's input and check if the input is blank
+        // If not add to the arraylist, otherwise change the temp variable to break from the loop
+        // return the arraylist
+    }
+
+    public String findWinner(ArrayList<String> contestantList) {
+        // Create a random object and have the upper bound the size of contestantList
+        // Generate a random number
+        // Save the name of the contestant at randomNum to a string called contestantName
+        // Return a string stating "The winner is... contestantName."
+    }
+}
